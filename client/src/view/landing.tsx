8ab37e4d539152bedc3aaffba8972ba5e6e880e7
asdf <qwer>
2023-03-14 11:37:53 -0500

    added branding logo and favicon

--- client/src/view/landing.tsx
+++ client/src/view/landing.tsx
@@ -26,14 +26,28 @@ export const LandingView: React.FC<{ vm: DataVM, isCustom: boolean}> = ({vm, isC
     const onApplyClick = async () => await vm.onInitialCustom(customLocation)
     const onInputChange = (_: React.ChangeEvent<HTMLInputElement>) => setCustomLocation(_.target.value)
 
+      const logoStyle: CSS.Properties = {
+            maxWidth: "100%",
+            height: "auto",
+            width: "25vh",
+            padding: 0
+        }
+    const childStyle: CSS.Properties = { color: "white",  fontSize: "1.2vmax", paddingTop: "1vm" }
+
     return <div style={landingStyle} id="flex_column">
         <h1 style={{fontSize: "4vmax"}}>Ephemerus.io</h1>
         <p style={{fontSize: "2.5vmax"}}>Adventure Awaits</p>
+        <div id="flex_column">
+            <img style={logoStyle} src={process.env.REACT_APP_BASE_URL+":8080/logo.jpg"} alt="todo"/>
+        </div>
         {
             vm.state.isLoading ? <LoadingSpinner msg={"Loading"}/>
             : vm.state.error ? <ComponentErrorView err={vm.state.error}/>
             : vm.state.gameData ? <Navigate to="/play" replace={false} />
-            : !isCustom ? <Button txt={"Start"} doOnClick={onStartClick}/>
+            : !isCustom ? <>
+                  <Button txt={"Start"} doOnClick={onStartClick}/>
+                  <a style={childStyle} href="/custom">custom</a>
+              </>
             : <>
                   <input type="text" placeholder={hintText} onChange={onInputChange}/>
                   <Button txt={"Apply"} doOnClick={onApplyClick}/>
